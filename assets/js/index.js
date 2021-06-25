ac187dae4fac9d3620ae2d3433311c90fe241160
anaismiraira <mirairaanais@gmail.com>
2021-06-25 12:49:58 -0400

    1249 25062021 #4 detalles en desktop Home

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,3 +40,27 @@ $(document).ready(function() {
   $('#precontact').load('components/precontact.html');
   $('#footer').load('components/footer.html');
 });
+
+$(document).ready(function() {
+
+  /* This is basic - uses default settings */
+
+  $("a#single_image").fancybox();
+
+  /* Using custom settings */
+
+  // $("a#inline").fancybox({
+  //   'hideOnContentClick': true
+  // });
+
+  /* Apply fancybox to multiple items */
+
+  // $("a.group").fancybox({
+  //   'transitionIn'  :  'elastic',
+  //   'transitionOut'  :  'elastic',
+  //   'speedIn'    :  600,
+  //   'speedOut'    :  200,
+  //   'overlayShow'  :  false
+  // });
+
+});

f2a063c2cb0af6759dc2c2b42d7c45c148595c6f
alexjhoan <alexjhoan.25@gmail.com>
2021-07-09 19:54:26 -0400

    menor changes 2000 09072021

--- assets/js/index.js
+++ assets/js/index.js
@@ -66,3 +66,60 @@ $(document).ready(function() {
 });
 
 
+// --------------------------form-------------------------------
+function dataSubmited(data) {
+  const requestOptions = {
+    method: 'POST',
+    body: data,
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+  };
+  fetch("https://www.infocasas.com.uy?mid=formulario&func=ajax_save&json=1", requestOptions)
+  .then((json) => {
+    setTimeout(()=>{
+      if (json.status === 200) {
+        $('#formSuccess').fadeIn();
+      } else {
+        $('#formError').fadeIn();
+      }
+      $('#formSending').hide();
+    }, 2000)
+  })
+  .catch(error => {
+    console.log('error', error);
+    setTimeout(() => {
+      $('#formSending').hide();
+      $('#formError').fadeIn();
+    }, 100)
+  });
+}
+
+function submited() {
+  const form = document.querySelector('#contactForm')
+  const data = JSON.stringify({
+    nombre: form.name.value,
+    apellido: "",
+    email: form.email.value,
+    telefono: form.phone.value,
+    tel: form.phone.value,
+    source: 2,
+    utm_source: "web_oga",
+    extra: form.consult.value,
+    IDpais: 2,
+    IDform: 583
+  })
+  event.preventDefault()
+  if (!form.checkValidity()) {
+    event.stopPropagation()
+  }else{
+    dataSubmited(data)
+    setTimeout(()=>{
+      $(form).fadeOut();
+      $('#formSending').fadeIn();
+    },300)
+  }
+  form.classList.add('was-validated')
+}
+
+
